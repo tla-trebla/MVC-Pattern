ce197d3f02ba38324293e6c1b6af82b98611f19b
Albert Pangestu <stamina.left@gmail.com>
2021-09-30 01:15:17 +0700

    Added label outlets.
    Added outlet labels for answer, correct count, incorrect count, prompt, and hint.

--- RabbleWabble/RabbleWabble/Views/QuestionView.swift
+++ RabbleWabble/RabbleWabble/Views/QuestionView.swift
@@ -5,4 +5,12 @@
 //  Created by Albert Pangestu on 30/09/21.
 //
 
-import Foundation
+import UIKit
+
+public class QuestionView: UIView {
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+}
