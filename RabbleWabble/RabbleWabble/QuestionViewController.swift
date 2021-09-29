6a3f9871eb94a510edc23025e59082902bac87a1
Albert Pangestu <stamina.left@gmail.com>
2021-09-29 22:30:15 +0700

    Renamed ViewController into QuestionViewController.

--- RabbleWabble/RabbleWabble/QuestionViewController.swift
+++ RabbleWabble/RabbleWabble/QuestionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionViewController.swift
 //  RabbleWabble
 //
 //  Created by Albert Pangestu on 29/09/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
