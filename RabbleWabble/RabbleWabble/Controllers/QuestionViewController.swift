ad928abbb9a34cd5c49a38db42d80083ac92c35e
Albert Pangestu <stamina.left@gmail.com>
2021-09-30 23:19:35 +0700

    Added properties and computed property.
    Properties named "questionGroup" to get some sample data.
    
    "questionIndex" to check current question number.
    
    "correctCount" & "incorrectCount" are self explained.
    
    Computed property named "questionView" to check if view with subclass "QuestionView" is already loaded.

--- RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 public class QuestionViewController: UIViewController {
+    
+    // MARK: - Instance properties
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
+    
+    public var correctCount = 0
+    public var incorrectCount = 0
+    
+    public var questionView: QuestionView! {
+        guard isViewLoaded else {
+            return nil
+        }
+        return (view as! QuestionView)
+    }
 
     public override func viewDidLoad() {
         super.viewDidLoad()
